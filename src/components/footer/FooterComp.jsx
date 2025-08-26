8ce5928bd9d9b45103cede3b266e8819b25f4d66
TaherMandsor53 <tahermandsorwala29@gmail.com>
2025-08-26 17:54:17 +0530

    Added copyright year

--- src/components/footer/FooterComp.jsx
+++ src/components/footer/FooterComp.jsx
@@ -2,12 +2,14 @@ import React from 'react';
 import { CopyrightCircleOutlined } from '@ant-design/icons';
 
 export default function FooterComp() {
+  const currentYear = new Date().getFullYear();
+
   return (
     <div className="footer-main">
       <div>
         SUTTARWALA CONSTRUCTION
         <CopyrightCircleOutlined className="copy-right-icon" />
-        2024
+        {currentYear}
       </div>
     </div>
   );
